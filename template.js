6d93cf2f8e8a07c86c4c22b2dc4de82eaf025d40
jeremyc2 <goldenmeso@gmail.com>
2018-04-02 21:07:35 -0600

    Update template.js

--- template.js
+++ template.js
@@ -4,13 +4,12 @@ exports.get = function(req, res) {
 
 	//var fields = Object.keys(Author.schema.obj);
 	var fields = [
-		'reference',
-		''
+		'reference'
 	];
 
 	var csv = json2csv({ data: '', fields: fields });
 
-	res.set("Content-Disposition", "attachment;filename=authors.csv");
+	res.set("Content-Disposition", "attachment;filename=scriptures.csv");
 	res.set("Content-Type", "application/octet-stream");
 
 	res.send(csv);
